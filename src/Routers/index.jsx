3c284a76a5796a17942489caaa63b3312b6e75ee
arthay <arthur15251@gmail.com>
2020-04-15 16:11:45 +0400

    add templates route

--- src/Routers/index.jsx
+++ src/Routers/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
-import { Sidebar, FreeToEdit } from '@PicsArtWeb/react-ui-library';
+import { Sidebar, FreeToEdit, Templates } from '@PicsArtWeb/react-ui-library';
 
-const array = [{
-    link: '/free-to-edit',
-    text: 'Free To Edit'
-}, {
-
-    link: '/my-profile',
-    text: 'My Profile'
-}];
+const array = [
+    {
+        link: '/free-to-edit',
+        text: 'Free To Edit'
+    },
+    {
+        link: '/templates',
+        text: 'Templates'
+    },
+    {
+        link: '/my-profile',
+        text: 'My Profile'
+    }
+];
 
 export function Routers({ children }) {
+
+    const onClick = () => {
+        console.log('do something');
+    };
     return (
         <section className='chooser'>
             <Sidebar >
@@ -20,6 +30,11 @@ export function Routers({ children }) {
             <Switch>
                 <Route path={'/free-to-edit'}>
                     <FreeToEdit />
+                </Route>
+                <Route path={'/templates'}>
+                    <Templates
+                        onTemplateClick={onClick}
+                    />
                 </Route>
                 <Route path={'/my-profile'}>
                     <div> Profile </div>
