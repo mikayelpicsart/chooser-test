306b5a394d7c383fde13bd21f60e1b39d7ddc5ac
Mikayel Abrahamyan <ma@MikayelAbrahamyanQ2.local>
2020-04-29 19:19:58 +0400

    fix

--- src/components/Chooser/index.jsx
+++ src/components/Chooser/index.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useCallback, useContext } from 'react';
-import { useHistory, Route, useRouteMatch } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { useHistory, Route, Switch } from 'react-router-dom';
 import { addData } from '../../services/IndexedDbService';
 import {
     FreeToEdit,
@@ -9,24 +9,11 @@ import {
     Upload,
     Link as ChooserLink,
     Selected,
-    ChooserActionProvider,
-    ChooserContext
+    ChooserActionProvider
 } from '@PicsArtWeb/react-ui-library';
 
-function RouteCustom({ path, children }) {
-    const match = useRouteMatch(path);
-    return React.cloneElement(children, { hidden: !match });
-}
-
 export function Chooser() {
-    const [{ images = [] } = {}] = useContext(ChooserContext);
     const history = useHistory();
-
-    useEffect(() => {
-        if (!images.length) {
-            history.push('/');
-        }
-    }, [images.length, history]);
 
     const onNextClick = useCallback(async (images) => {
         for (const image of images) {
@@ -34,34 +21,34 @@ export function Chooser() {
 
             await addData(image);
         }
-
         history.push('/process');
     }, [history]);
 
     const searchUrl = 'https://api.picsart.com/photos/search.json?q=origfte,people,person';
 
     return (<ChooserActionProvider onNextClick={onNextClick}>
-        <RouteCustom path={'/free_to_edit'}>
-            <FreeToEdit searchUrl={searchUrl}/>
-        </RouteCustom>
-        <Route path={'/templates'}>
-            <Templates onTemplateClick={(test) => console.log(test)} />
-        </Route>
-        <RouteCustom path={'/my_profile'}>
-            <MyProfile userId={98050114} />
-        </RouteCustom>
-        <RouteCustom path={'/my_collections'}>
-            <MyCollections userId={98050114} />
-        </RouteCustom>
-
-        <Route path={'/link'}>
-            <ChooserLink />
-        </Route>
-        <Route path={'/selected'}>
-            <Selected />
-        </Route>
-        <Route exact path={'/'}>
-            <Upload />
-        </Route>
+        <Switch>
+            <Route path={'/free_to_edit'}>
+                <FreeToEdit searchUrl={searchUrl} />
+            </Route>
+            <Route path={'/templates'}>
+                <Templates onTemplateClick={(test) => console.log(test)} />
+            </Route>
+            <Route path={'/my_profile'}>
+                <MyProfile userId={98050114} />
+            </Route>
+            <Route path={'/my_collections'}>
+                <MyCollections userId={98050114} />
+            </Route>
+            <Route path={'/link'}>
+                <ChooserLink />
+            </Route>
+            <Route path={'/selected'}>
+                <Selected />
+            </Route>
+            <Route exact path={'/'}>
+                <Upload />
+            </Route>
+        </Switch>
     </ChooserActionProvider>);
 }
